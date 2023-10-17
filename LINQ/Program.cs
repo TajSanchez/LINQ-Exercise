1e4ede5419c1b23a7cd56ce55b1f01dec3101b3b
Taj <tajmarie92@gmail.com>
2023-10-17 11:23:47 -0500

    Completed Exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace LINQ
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using System.Linq;
+Console.WriteLine("Video game collection");
+
+List<string> videoGames = new List<string>{"Resident Evil 4", "Assassin's Creed Revelations", "Watch Dogs", "Dishonored" };
+var orderedVideoGames = videoGames.OrderBy(x => x.Length).ToList();
+
+videoGames.ForEach(videoGame =>  Console.WriteLine(videoGame));
+Console.WriteLine("__________________");
+orderedVideoGames.ForEach(videoGame =>  Console.WriteLine(videoGame));
